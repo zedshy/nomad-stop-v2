f92fdbba16f95132a04db721d529ab8ebdff0454
Zeshaan Qureshi <zeshaanqureshi@MacBookPro.lan>
2025-12-08 15:31:54 +0000

    Add script to run price update with custom temp dir

---
+++ run-price-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run price update with custom temp directory
+# This avoids the /tmp permission issue
+
+cd /var/www/nomad-stop
+
+# Set temp directory to a writable location
+export TMPDIR=/var/www/nomad-stop/tmp
+mkdir -p "$TMPDIR"
+
+# Run the update script
+npm run prices:update-all
+
